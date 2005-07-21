ef39465dded68e9682ca32bf12e1af582ed307cb
Alan Knowles <alan_k@php.net>
2005-07-21 06:33:36 +0000

    fix bug #4834 - case sensitive checks for db_dataobject_cast fail
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_DataObject/trunk@191110 c90b9560-bf6c-de11-be94-00142212c4b1

--- DataObject.php
+++ DataObject.php
@@ -936,7 +936,7 @@ class DB_DataObject extends DB_DataObject_Overload
             
             $leftq .= ($quoteIdentifiers ? ($DB->quoteIdentifier($k) . ' ')  : "$k ");
             
-            if (is_a($this->$k,'db_dataobject_cast')) {
+            if (is_a($this->$k,'DB_DataObject_Cast')) {
                 $value = $this->$k->toString($v,$DB);
                 if (PEAR::isError($value)) {
                     $this->raiseError($value->getMessage() ,DB_DATAOBJECT_ERROR_INVALIDARG);
@@ -1171,7 +1171,7 @@ class DB_DataObject extends DB_DataObject_Overload
             
             $kSql = ($quoteIdentifiers ? $DB->quoteIdentifier($k) : $k);
             
-            if (is_a($this->$k,'db_dataobject_cast')) {
+            if (is_a($this->$k,'DB_DataObject_Cast')) {
                 $value = $this->$k->toString($v,$DB);
                 if (PEAR::isError($value)) {
                     $this->raiseError($value->getMessage() ,DB_DATAOBJECT_ERROR_INVALIDARG);
@@ -2342,7 +2342,7 @@ class DB_DataObject extends DB_DataObject_Overload
              
              
             
-            if (is_a($this->$k,'db_dataobject_cast')) {
+            if (is_a($this->$k,'DB_DataObject_Cast')) {
                 $dbtype = $DB->dsn["phptype"];
                 $value = $this->$k->toString($v,$DB);
                 if (PEAR::isError($value)) {
