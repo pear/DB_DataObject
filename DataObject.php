484d9f7dca0e771ffd205c1412df3a90770ef736
Alan Knowles <alan_k@php.net>
2005-11-10 03:06:40 +0000

    make debugging schema loading errors a bit easier
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_DataObject/trunk@200320 c90b9560-bf6c-de11-be94-00142212c4b1

--- DataObject.php
+++ DataObject.php
@@ -1775,8 +1775,10 @@ class DB_DataObject extends DB_DataObject_Overload
             // should this fail!!!???
             return true;
         }
+        $this->debug("Cant find database schema: {$this->_database}/{$this->__table} \n".
+                    "in links file data: " . print_r($_DB_DATAOBJECT['INI'],true),"databaseStructure",5);
         // we have to die here!! - it causes chaos if we dont (including looping forever!)
-        $this->raiseError( "Unable to load schema for database and table", DB_DATAOBJECT_ERROR_INVALIDARGS, PEAR_ERROR_DIE);
+        $this->raiseError( "Unable to load schema for database and table (turn debugging up to 5 for full error message)", DB_DATAOBJECT_ERROR_INVALIDARGS, PEAR_ERROR_DIE);
         return false;
         
          
