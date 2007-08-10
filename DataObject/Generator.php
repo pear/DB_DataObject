a822f5be183d297dc02d856cca13210e5c72a2a5
Alan Knowles <alan_k@php.net>
2007-08-10 03:59:30 +0000

    fix bug #9658 - postgres DECIMAL using DB backend incorrectly flagged as unknown - changed warning to suggest using MDB2
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_DataObject/trunk@241002 c90b9560-bf6c-de11-be94-00142212c4b1

--- DataObject/Generator.php
+++ DataObject/Generator.php
@@ -608,6 +608,9 @@ class DB_DataObject_Generator extends DB_DataObject
                          "** Found column '{$t->name}', of type  '{$t->type}'            **\n".
                          "** Please submit a bug, describe what type you expect this     **\n".
                          "** column  to be                                               **\n".
+                         "** ---------POSSIBLE FIX / WORKAROUND -------------------------**\n".
+                         "** Try using MDB2 as the backend - eg set the config option    **\n".
+                         "** db_driver = MDB2                                            **\n".
                          "*****************************************************************\n";
                     $write_ini = false;
                     break;
