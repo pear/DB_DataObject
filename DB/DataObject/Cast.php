68ef6d07e496f00f951cb306c2faf3b607cdfc5a
Alan Knowles <alan_k@php.net>
2012-07-12 02:58:28 +0000

    fix #19500 - use same way that pear db mssql driver quotes for regular strings (tidy up)
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_DataObject/trunk@326603 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/DataObject/Cast.php
+++ DB/DataObject/Cast.php
@@ -396,7 +396,14 @@ class DB_DataObject_Cast {
                 return "'".sqlite_escape_string($this->value)."'";
            
             case 'mssql':
-                return $this->escapeMSsql($this->value);
+                
+                if(is_numeric($this->value)) {
+                    return $this->value;
+                }
+                $unpacked = unpack('H*hex', $this->value);
+                return '0x' . $unpacked['hex'];
+                        
+                 
    
             default:
                 return PEAR::raiseError("DB_DataObject_Cast cant handle blobs for Database:{$db->dsn['phptype']} Yet");
@@ -441,7 +448,13 @@ class DB_DataObject_Cast {
                 return "'".mysqli_real_escape_string($db->connection, $this->value)."'";
 
             case 'mssql':
-                return $this->escapeMSsql($this->value);
+                // copied from the old DB mssql code...?? not sure how safe this is.
+                return "'" . str_replace(
+                        array("'", "\\\r\n", "\\\n"),
+                        array("''", "\\\\\r\n\r\n", "\\\\\n\n"),
+                        $this->value 
+                    ) . "'";
+                
 
             default:
                 return PEAR::raiseError("DB_DataObject_Cast cant handle blobs for Database:{$db->dsn['phptype']} Yet");
@@ -545,19 +558,7 @@ class DB_DataObject_Cast {
     {
         return $this->value; 
     }
-    /**
-     * workaround for lack of quoting in mssql api.
-     * http://stackoverflow.com/questions/574805/how-to-escape-strings-in-mssql-using-php
-     * 
-     */
-    function escapeMSsql($data)
-    {
-        if(is_numeric($data)) {
-            return $data;
-        }
-        $unpacked = unpack('H*hex', $data);
-        return '0x' . $unpacked['hex'];
-    }
+    
     
     
 }
